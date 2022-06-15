0ffbd553f53e51003246818d5bc16dbea936af61
AzenaideFernandes <azenaidefkt@gmail.com>
2022-06-15 12:52:29 -0400

    primeiro commit
    commit geral

---
+++ mobile/src/components/Copywight/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './styles';
+
+export function Copywight() {
+  return (
+    <View >
+      <Text style={styles.text}>
+        Feito com ♥ pela Rocketseat
+
+      </Text>
+
+    </View>
+  );
+}
